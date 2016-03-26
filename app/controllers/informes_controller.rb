ea4fd64a19dda9351f426ce30666be5f0d509369
Francisco Cogollo <fjcogollor@gmail.com>
2016-03-26 00:58:27 -0500

    Controlador informe con vistas interconectadas funcionales.

--- app/controllers/informes_controller.rb
+++ app/controllers/informes_controller.rb
@@ -1,15 +1,30 @@
 class InformesController < ApplicationController
+
  def index
- @informes=Informes.all
+ @informe=Informe.all
  end
  
  def show
- #@informes=Informes.find(params[:id])
+ @informe=Informe.find(params[:id])
  end 
  def new
- 
+  @informe=Informe.new
+  @informe.num_autos_parqueados = Parqueo.count(:hora_salida)
+  @informe.num_autos_inscritos = Automovil.where(inscrito: true).count
+  @informe.total_recaudo = Parqueo.sum("valor_servicio").to_i
+  @informe.fecha = Time.new 
+   
  end
+ 
  def create
  
+ @informe=Informe.new(informe_params)
+ @informe.save
+ redirect_to @informe
  end
+ 
+ private
+  def informe_params
+    params.require(:informe).permit(:num_autos_parqueados,:num_autos_inscritos,:total_recaudo,:fecha)
+  end
 end
